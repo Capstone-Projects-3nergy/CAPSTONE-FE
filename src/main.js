5a6c5a66d01d39761ec4ad890e65a05c0e06d1d5
นายกฤตภาส จารุบูรณศักดิ์-65130500092 <krittapas22754@gmail.com>
2025-10-27 01:40:56 +0700

    install pinia libary

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
+import './assets/style.css'
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from '../router/index.js'
-import './assets/style.css'
-// import { createPinia } from 'pinia'
+import { createPinia } from 'pinia'
 const app = createApp(App)
-// app.use(createPinia())
+app.use(createPinia())
 app.use(router)
 app.mount('#app')
 // createApp(App).mount('#app')
