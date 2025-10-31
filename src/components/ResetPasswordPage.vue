af9f1efe565b8eabcad18032b127b8f73f5f9d57
นายกฤตภาส จารุบูรณศักดิ์-65130500092 <krittapas22754@gmail.com>
2025-10-31 18:01:31 +0700

    Update ResetPasswordPage.vue

--- src/components/ResetPasswordPage.vue
+++ src/components/ResetPasswordPage.vue
@@ -350,7 +350,7 @@ const checkInputLength = (field) => {
             <input
               type="email"
               v-model="email"
-              placeholder="you@example.com"
+              placeholder="Email"
               class="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-400 transition-colors"
               required
             />
