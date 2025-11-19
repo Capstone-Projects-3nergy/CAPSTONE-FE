681477300616022562b6f4fbebaf3597392aab60
นายกฤตภาส จารุบูรณศักดิ์-65130500092 <krittapas22754@gmail.com>
2025-11-19 16:02:04 +0700

    Update LoginPage.vue

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -370,16 +370,16 @@ const showResetPasswordPageWeb = async function () {
   <div class="min-h-screen flex flex-col md:flex-row">
     <!-- Left Section -->
     <div
-      class="hidden md:block flex-1 bg-gradient-to-b from-[#0047b1] to-[#7bb8ff] text-white flex-col justify-center items-center p-4 h-20 sm:h-28 md:h-36 lg:min-h-screen"
+      class="hidden md:flex flex-1 bg-gradient-to-b from-[#0047b1] to-[#7bb8ff] text-white flex-col justify-center items-center p-4 h-20 sm:h-28 md:h-36 lg:min-h-screen"
     >
-      <div class="max-w-xs text-center md:text-left">
-        <h1 class="text-2xl sm:text-3xl font-bold mb-3">Welcome back!</h1>
-        <p class="text-xs sm:text-sm text-white mb-6">
+      <div class="max-w-md text-center md:text-center">
+        <h1 class="text-3xl font-bold mb-4 md:text-center">Welcome back!</h1>
+        <p class="text-sm text-white mb-8 md:text-left">
           dormitory parcel management system — manage, check status, and stay
           updated anytime with Tractify.
         </p>
-
-        <div class="flex justify-center">
+        <!-- ปรับตรงกลางเฉพาะบน desktop -->
+        <div class="flex md:justify-center">
           <svg
             class="hidden md:block w-[490px] h-[569px]"
             viewBox="0 0 490 569"
