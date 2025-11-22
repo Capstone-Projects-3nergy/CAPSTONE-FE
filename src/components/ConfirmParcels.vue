9d64d69a54b948981258c422b6fbc415aa32cd41
นายกฤตภาส จารุบูรณศักดิ์-65130500092 <krittapas22754@gmail.com>
2025-11-22 12:30:31 +0700

    add confirm popup

---
+++ src/components/ConfirmParcels.vue
@@ -0,0 +1,91 @@
+<script setup>
+import { ref, computed, onMounted } from 'vue'
+import { useParcelManager } from '@/stores/ParcelsManager'
+import { useRouter } from 'vue-router'
+import ButtonWeb from './ButtonWeb.vue'
+import { deleteItemById } from '@/utils/fetchUtils'
+
+const emit = defineEmits(['confirmDetail', 'cancelDetail', 'redAlert'])
+const props = defineProps(['parcelData']) // ไม่ใช่ ref
+
+const router = useRouter()
+const parcelManager = useParcelManager()
+const parcelEditDetail = ref(null)
+const parcelIdDetail = ref(null)
+const deletedParcel = ref(null)
+// onMounted(async () => {
+//   isCollapsed.value = true
+// })
+// ใช้ computed เผื่อ props เป็น undefined
+const parcel = computed(() => props.parcelData || {})
+
+const deleteParcelFn = async () => {
+  if (!parcel.value.id) return
+
+  deletedParcel.value = await deleteItemById(
+    `${import.meta.env.VITE_BASE_URL}/api/parcels`,
+    parcel.value.id
+  )
+
+  if (deletedParcel.value === '404') {
+    emit('redAlert')
+    emit('cancelDetail', true)
+    return
+  }
+
+  // ลบใน Pinia
+  parcelManager.deleteParcels(parcel.id)
+
+  emit('confirmDetail', true)
+}
+
+const cancelFn = () => {
+  emit('cancelDetail', true)
+  router.replace({ name: 'staffparcels' })
+}
+</script>
+
+<template>
+  <div
+    class="message bg-gray-500/50 backdrop-blur-sm w-screen h-screen fixed top-0 left-0 flex items-center justify-center p-2"
+  >
+    <div
+      class="bg-white border border-green-700 w-full max-w-lg rounded-md overflow-hidden"
+    >
+      <!-- Header -->
+      <div class="flex flex-col justify-between p-4 border-b">
+        <h1 class="text-xl font-bold text-center sm:text-left">
+          Delete Parcel
+        </h1>
+      </div>
+
+      <!-- Body -->
+      <div class="p-4 text-center sm:text-left">
+        <p class="mb-4">
+          Are you sure you want to confirm the receipt of the parcel with
+          tracking number
+          <b>{{ parcel.parcelNumber || '' }}</b
+          >?
+        </p>
+      </div>
+
+      <!-- Footer -->
+      <div class="flex flex-col sm:flex-row justify-end gap-2 p-4 border-t">
+        <ButtonWeb
+          label="Confirm"
+          color="green"
+          class="w-full sm:w-auto"
+          @click="deleteParcelFn"
+        />
+        <ButtonWeb
+          label="Cancel"
+          color="red"
+          class="w-full sm:w-auto"
+          @click="cancelFn"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
