ec131e131bac9bfb0190bb165c424d73e9328247
นายกฤตภาส จารุบูรณศักดิ์-65130500092 <krittapas22754@gmail.com>
2025-11-25 23:24:50 +0700

    add tooggle sort

--- src/components/ResidentParcels.vue
+++ src/components/ResidentParcels.vue
@@ -1258,8 +1258,8 @@ const showProfileResidentPage = async function () {
                 <option>Name (Z→A)</option>
                 <option>Room (A→Z)</option>
                 <option>Room (Z→A)</option> -->
-                <option>Status (A→Z)</option>
-                <option>Status (Z→A)</option>
+                <!-- <option>Status (A→Z)</option>
+                <option>Status (Z→A)</option> -->
               </select>
 
               <!-- Add Parcel -->
@@ -1334,16 +1334,85 @@ const showProfileResidentPage = async function () {
                   Name
                 </th>
                 <th class="px-4 py-3 text-sm font-semibold text-[#185DC0]">
-                  Room
+                  <div class="flex items-center gap-2">
+                    Room
+                    <svg
+                      class="cursor-pointer"
+                      @click="toggleSortRoom"
+                      width="17"
+                      height="12"
+                      viewBox="0 0 17 12"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M0.75 0.75H15.75H0.75ZM3.25 5.75H13.25H3.25ZM6.25 10.75H10.25H6.25Z"
+                        fill="#185DC0"
+                      />
+                      <path
+                        d="M0.75 0.75H15.75M3.25 5.75H13.25M6.25 10.75H10.25"
+                        stroke="#5C9BEB"
+                        stroke-width="1.5"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                    </svg>
+                  </div>
                 </th>
                 <th class="px-4 py-3 text-sm font-semibold text-[#185DC0]">
                   Email
                 </th>
                 <th class="px-4 py-3 text-sm font-semibold text-[#185DC0]">
-                  Status
+                  <div class="flex items-center gap-2">
+                    Status
+                    <svg
+                      class="cursor-pointer"
+                      @click="toggleSortStatus"
+                      width="17"
+                      height="12"
+                      viewBox="0 0 17 12"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M0.75 0.75H15.75H0.75ZM3.25 5.75H13.25H3.25ZM6.25 10.75H10.25H6.25Z"
+                        fill="#185DC0"
+                      />
+                      <path
+                        d="M0.75 0.75H15.75M3.25 5.75H13.25M6.25 10.75H10.25"
+                        stroke="#5C9BEB"
+                        stroke-width="1.5"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                    </svg>
+                  </div>
                 </th>
                 <th class="px-4 py-3 text-sm font-semibold text-[#185DC0]">
-                  Receive At
+                  <div class="flex items-center gap-2">
+                    Receive At
+                    <svg
+                      class="cursor-pointer"
+                      @click="toggleSortDate"
+                      width="17"
+                      height="12"
+                      viewBox="0 0 17 12"
+                      fill="none"
+                      xmlns="http://www.w3.org/2000/svg"
+                    >
+                      <path
+                        d="M0.75 0.75H15.75H0.75ZM3.25 5.75H13.25H3.25ZM6.25 10.75H10.25H6.25Z"
+                        fill="#185DC0"
+                      />
+                      <path
+                        d="M0.75 0.75H15.75M3.25 5.75H13.25M6.25 10.75H10.25"
+                        stroke="#5C9BEB"
+                        stroke-width="1.5"
+                        stroke-linecap="round"
+                        stroke-linejoin="round"
+                      />
+                    </svg>
+                  </div>
                 </th>
                 <th
                   class="px-4 py-3 text-sm text-center font-semibold text-[#185DC0]"
