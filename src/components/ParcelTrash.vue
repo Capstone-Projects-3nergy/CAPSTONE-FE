81f3b260301aadc181ede6c89d7996ea139ba150
นายกฤตภาส จารุบูรณศักดิ์-65130500092 <krittapas22754@gmail.com>
2025-11-26 20:52:16 +0700

    Update ParcelTrash.vue

--- src/components/ParcelTrash.vue
+++ src/components/ParcelTrash.vue
@@ -1,29 +1,34 @@
 <script setup>
 import { ref, onMounted } from 'vue'
 import { useParcelStore } from '@/stores/parcelStore'
-
+import ButtonWeb from './ButtonWeb.vue'
+import AlertPopUp from './AlertPopUp.vue'
 const parcelStore = useParcelStore()
 const trashList = ref([])
-
+const deleteSuccess = ref(false)
+const error = ref(false)
 const fetchTrash = async () => {
   const res = await fetch(`${import.meta.env.VITE_BASE_URL}/api/parcels/trash`)
   trashList.value = await res.json()
 }
 
-const restoreParcel = async (id) => {
-  const res = await fetch(
-    `${import.meta.env.VITE_BASE_URL}/api/parcels/${id}/restore`,
-    { method: 'PUT' }
-  )
+// const restoreParcel = async (id) => {
+//   const res = await fetch(
+//     `${import.meta.env.VITE_BASE_URL}/api/parcels/${id}/restore`,
+//     { method: 'PUT' }
+//   )
 
-  if (res.ok) {
-    await fetchTrash()
-  }
-}
+//   if (res.ok) {
+//     await fetchTrash()
+//   }
+// }
 
 const deleteForever = async (id) => {
-  if (!confirm('Delete permanently? This action cannot be undone.')) return
-
+  if (!confirm('Delete permanently? This action cannot be undone.')) {
+    error.value = true
+    setTimeout(() => (error.value = false), 3000)
+    return
+  }
   const res = await fetch(
     `${import.meta.env.VITE_BASE_URL}/api/parcels/${id}/force`,
     { method: 'DELETE' }
@@ -31,10 +36,19 @@ const deleteForever = async (id) => {
 
   if (res.ok) {
     await fetchTrash()
+    setTimeout(() => (deleteSuccess.value = false), 3000)
+    deleteSuccess.value = true
   }
 }
 
 onMounted(fetchTrash)
+const cancelPage = () => {
+  router.replace({ name: 'detailparcels' })
+}
+const closePopUp = (operate) => {
+  if (operate === 'problem') error.value = false
+  if (operate === 'errorSuccessMessage ') addSuccess.value = false
+}
 </script>
 
 <template>
@@ -43,7 +57,25 @@ onMounted(fetchTrash)
 
     <!-- ไม่มีข้อมูล -->
     <div v-if="trashList.length === 0" class="text-gray-500 text-center py-10">
-      ไม่มีพัสดุในถังขยะ
+      No Parcels In Trash
+      <div class="fixed top-5 left-5 z-50">
+        <AlertPopUp
+          v-if="deleteSuccess"
+          :titles="'Add New Parcel is Successfull.'"
+          message="Success!!"
+          styleType="green"
+          operate="errorSuccessMessage"
+          @closePopUp="closePopUp"
+        />
+        <AlertPopUp
+          v-if="error"
+          :titles="'There is a problem. Please try again later.'"
+          message="Error!!"
+          styleType="red"
+          operate="problem"
+          @closePopUp="closePopUp"
+        />
+      </div>
     </div>
 
     <!-- ตาราง -->
@@ -73,24 +105,42 @@ onMounted(fetchTrash)
 
             <td class="px-4 py-2 flex gap-2">
               <!-- Restore -->
-              <button
+              <!-- <button
                 @click="restoreParcel(p.id)"
                 class="px-3 py-1 bg-green-500 text-white rounded-lg hover:bg-green-600"
               >
                 Restore
-              </button>
+              </button> -->
+              <!-- <ButtonWeb
+                label=" Restore"
+                color="green"
+                @click="restoreParcel(p.id)"
+                class="w-full md:w-auto"
+              /> -->
 
               <!-- Delete forever -->
-              <button
+              <!-- <button
                 @click="deleteForever(p.id)"
                 class="px-3 py-1 bg-red-500 text-white rounded-lg hover:bg-red-600"
               >
                 Delete Permanently
-              </button>
+              </button> -->
+              <ButtonWeb
+                label="Delete Permanently"
+                color="red"
+                @click="deleteForever(p.id)"
+                class="w-full md:w-auto"
+              />
             </td>
           </tr>
         </tbody>
       </table>
     </div>
+    <ButtonWeb
+      label="Cancel"
+      color="gray"
+      @click="cancelPage"
+      class="w-full md:w-auto"
+    />
   </div>
 </template>
