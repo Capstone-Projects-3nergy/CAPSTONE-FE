0f64a7c03f98de7ff85f2293918878014fd095e2
นายกฤตภาส จารุบูรณศักดิ์-65130500092 <krittapas22754@gmail.com>
2025-11-29 01:46:28 +0700

    Update RegisterPage.vue

--- src/components/RegisterPage.vue
+++ src/components/RegisterPage.vue
@@ -551,8 +551,7 @@ const toggleComfirmPasswordVisibility = () => {
                   class="pl-10 w-full px-4 py-2 border rounded-lg focus:ring-2 focus:ring-blue-400 mb-3"
                   @input="checkInputLength('fullName')"
                   :class="{
-                    'border-red-600 text-red-600':
-                      isNameOverLimit || isFullNameWeak
+                    'border-red-600 text-red-600': isNameOverLimit
                   }"
                 />
                 <div
@@ -574,26 +573,6 @@ const toggleComfirmPasswordVisibility = () => {
                   </svg>
                   <div class="text-sm text-red-600">
                     Limit Full Name to 30 characters or less.
-                  </div>
-                </div>
-                <div
-                  v-if="isFullNameWeak"
-                  class="flex items-center text-sm text-red-600"
-                >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    fill="red"
-                    class="w-[15px] mr-1"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z"
-                      clip-rule="evenodd"
-                    />
-                  </svg>
-                  <div class="text-sm text-red-600">
-                    Full Name must be at least 6 characters.
                   </div>
                 </div>
               </div>
