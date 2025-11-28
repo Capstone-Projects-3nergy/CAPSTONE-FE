3ae4eb80cf1f51658ae87bafb60138c08e4e374d
นายกฤตภาส จารุบูรณศักดิ์-65130500092 <krittapas22754@gmail.com>
2025-11-28 23:09:17 +0700

    Update ParcelScannerPage.vue

--- src/components/ParcelScannerPage.vue
+++ src/components/ParcelScannerPage.vue
@@ -906,7 +906,7 @@ onMounted(async () => {
             <div class="space-y-6">
               <div
                 id="scanner"
-                class="w-full h-64 border-2 border-dashed border-blue-300 rounded-lg bg-black flex items-center justify-center relative overflow-hidden"
+                class="w-full h-58 sm:h-64 border-2 border-dashed border-blue-300 rounded-lg bg-black flex items-center justify-center relative overflow-hidden"
               >
                 <span v-if="!scanningMode && !videoStream" class="text-white">
                   Scan QR/Barcode or Take Picture
