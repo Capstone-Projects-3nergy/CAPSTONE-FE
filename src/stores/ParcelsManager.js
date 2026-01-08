061b20603685c1b0776c196b102627cc203f00f9
นายกฤตภาส จารุบูรณศักดิ์-65130500092 <krittapas22754@gmail.com>
2026-01-08 21:24:05 +0700

    Update ParcelsManager.js

--- src/stores/ParcelsManager.js
+++ src/stores/ParcelsManager.js
@@ -87,7 +87,7 @@ export const useParcelManager = defineStore('parcelManager', () => {
       trash.push({
         ...removed,
         original: { ...removed }, // เก็บสภาพก่อนลบ
-        deletedAt: new Date().toISOString()
+        updatedAt: new Date().toISOString()
       })
     }
   }
