4655dbdd2e79fc1faad2de32f61dae982f611c48
นายกฤตภาส จารุบูรณศักดิ์-65130500092 <krittapas22754@gmail.com>
2026-01-16 21:33:58 +0700

    Update PersonalInfoCard.vue

--- src/components/PersonalInfoCard.vue
+++ src/components/PersonalInfoCard.vue
@@ -233,8 +233,8 @@ const statusClass = (value) => {
             <svg
               class="cursor-pointer font-semibold hover:text-[#8C8F91] transition"
               @click="$emit('edit')"
-              width="24"
-              height="24"
+              width="20"
+              height="20"
               viewBox="0 0 24 24"
               fill="currentcolor"
               xmlns="http://www.w3.org/2000/svg"
