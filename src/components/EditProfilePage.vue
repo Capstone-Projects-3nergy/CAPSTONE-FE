4d3096175b8aa8af3372c0a25d71a0a7f35a301a
นายกฤตภาส จารุบูรณศักดิ์-65130500092 <krittapas22754@gmail.com>
2026-01-21 13:04:33 +0700

    Update EditProfilePage.vue

--- src/components/EditProfilePage.vue
+++ src/components/EditProfilePage.vue
@@ -807,7 +807,7 @@ const closePopUp = (operate) => {
           />
           <AlertPopUp
             v-if="phoneError"
-            :titles="'Phone number must have 9–10 digits (hyphens allowed).'"
+            :titles="'Phone number must have 9–10 digits (hyphens allowed but text not allowed).'"
             message="Error!!"
             styleType="red"
             operate="phoneMessage"
