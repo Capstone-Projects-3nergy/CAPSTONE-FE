ccff2b534e6b03b466be8c001713af66accc1f1e
นายกฤตภาส จารุบูรณศักดิ์-65130500092 <krittapas22754@gmail.com>
2026-01-27 21:10:38 +0700

    Update EditPersonalInfoProfile.vue

--- src/components/EditPersonalInfoProfile.vue
+++ src/components/EditPersonalInfoProfile.vue
@@ -900,6 +900,7 @@ const isSaveDisabled = computed(() => {
               v-model="forms.dormId"
               class="w-full bg-white border rounded-xl px-4 py-2.5 text-gray-800 focus:outline-none focus:ring-2 focus:ring-[#185DC0]"
             >
+              <option disabled value="null">Select Dormitory</option>
               <option
                 v-for="dorm in dormList"
                 :key="dorm.dormId"
