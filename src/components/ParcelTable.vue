ecd7a28f74df608d90b73d1ad2c37b125874b14e
นายกฤตภาส จารุบูรณศักดิ์-65130500092 <krittapas22754@gmail.com>
2026-02-06 15:10:06 +0700

    fix css empty text table

--- src/components/ParcelTable.vue
+++ src/components/ParcelTable.vue
@@ -261,7 +261,12 @@ const getInitial = (name) => {
 
       <tbody class="divide-y">
         <tr v-if="items.length === 0">
-          <td colspan="7" class="text-center py-6 text-gray-500 text-sm">
+          <td colspan="15" class="text-center py-12 text-gray-500 text-sm">
+            <div class="w-16 h-16 bg-gray-50 rounded-full flex items-center justify-center mx-auto mb-4">
+              <svg xmlns="http://www.w3.org/2000/svg" class="h-8 w-8 text-gray-300" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M20 13V6a2 2 0 00-2-2H6a2 2 0 00-2 2v7m16 0v5a2 2 0 01-2 2H6a2 2 0 01-2-2v-5m16 0h-2.586a1 1 0 00-.707.293l-2.414 2.414a1 1 0 01-.707.293h-3.172a1 1 0 01-.707-.293l-2.414-2.414A1 1 0 006.586 13H4" />
+              </svg>
+            </div>
             No data available
           </td>
         </tr>
