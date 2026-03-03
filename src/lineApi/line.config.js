8508305c50227132ba84e4bb0be86cfed43cd876
นายกฤตภาส จารุบูรณศักดิ์-65130500092 <krittapas22754@gmail.com>
2026-03-03 20:04:01 +0700

    and path in .env

--- src/lineApi/line.config.js
+++ src/lineApi/line.config.js
@@ -1,6 +1,6 @@
 // src/lineApi/line.config.js
 export const LINE_CONFIG = {
-  API_URL: '/api/line/send',
+  API_URL: import.meta.env.VITE_LINE_API_URL || '/api/line/send',
   CHANNEL_ID: import.meta.env.VITE_LINE_CHANNEL_ID,
   REDIRECT_URI: import.meta.env.VITE_LINE_REDIRECT_URI
 }
