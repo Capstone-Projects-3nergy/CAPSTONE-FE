ad6b3ca59ee65924adb810c45e908999b1355e38
นายกฤตภาส จารุบูรณศักดิ์-65130500092 <krittapas22754@gmail.com>
2026-03-20 23:07:54 +0700

    Update ParcelScannerPageSender.vue

--- src/components/ParcelScannerPageSender.vue
+++ src/components/ParcelScannerPageSender.vue
@@ -833,7 +833,7 @@ const closePopUp = (operate) => {
             </div>
             <h2 class="text-2xl md:text-3xl font-extrabold text-gray-900 tracking-tight">
               <span class="bg-clip-text text-transparent bg-gradient-to-r from-[#0E4B90] to-blue-600">
-                Manage Parcel &gt; Add
+                Add Parcel
               </span>
             </h2>
           </div>
