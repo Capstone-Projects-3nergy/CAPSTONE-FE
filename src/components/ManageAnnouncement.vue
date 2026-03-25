9807dc6cc4117b6cbd4b9295fbc29310a1c7b589
นายกฤตภาส จารุบูรณศักดิ์-65130500092 <krittapas22754@gmail.com>
2026-03-25 18:58:48 +0700

    Revert "Update ManageAnnouncement.vue"
    This reverts commit 1d99da27c510bb35e88efae6427a593732126b5f.

--- src/components/ManageAnnouncement.vue
+++ src/components/ManageAnnouncement.vue
@@ -60,7 +60,6 @@ const deleteSuccess = ref(false)
 const showPinLimitAlert = ref(false)
 const error = ref('')
 let statsInterval = null
-let statusUpdateInterval = null
 
 const closePopUp = (operate) => {
   if (operate === 'deleteSuccessMessage') {
@@ -313,9 +312,6 @@ onUnmounted(() => {
   if (statsInterval) {
     clearInterval(statsInterval)
   }
-  if (statusUpdateInterval) {
-    clearInterval(statusUpdateInterval)
-  }
 })
 onMounted(async () => {
   await Promise.all([
@@ -327,26 +323,6 @@ onMounted(async () => {
   statsInterval = setInterval(() => {
     fetchAnnouncementData()
   }, 30000)
-
-  // Set up status update check every second to auto-transition drafts to published
-  statusUpdateInterval = setInterval(() => {
-    const now = new Date()
-    let changed = false
-    announcements.value.forEach(ann => {
-      if (ann.status?.toLowerCase() === 'draft' && ann.publishAt) {
-        const publishDate = new Date(ann.publishAt)
-        if (!isNaN(publishDate.getTime()) && publishDate <= now) {
-          ann.status = 'PUBLISHED'
-          changed = true
-        }
-      }
-    })
-    
-    // If any status changed, we update the manager to keep it in sync
-    if (changed) {
-      announcementManager.setAnnouncements([...announcements.value])
-    }
-  }, 1000)
 })
 
 const showParcelScannerPage = async function () {
