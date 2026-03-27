8ed906757887c67a3fef0680338b8ac968625a40
นายกฤตภาส จารุบูรณศักดิ์-65130500092 <krittapas22754@gmail.com>
2026-03-27 17:55:41 +0700

    fix

--- src/components/CreateAnnouncement.vue
+++ src/components/CreateAnnouncement.vue
@@ -971,7 +971,7 @@ const returnLoginPage = async () => {
                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><circle cx="8.5" cy="8.5" r="1.5"></circle><polyline points="21 15 16 10 5 21"></polyline></svg>
                      </div>
                      <p class="text-sm font-medium text-gray-700 mt-2">Click to upload or drag file here</p>
-                     <p class="text-xs text-gray-500 font-medium">PNG, JPG, GIF max 1MB</p>
+                     <p class="text-xs text-gray-500 font-medium">PNG, JPG, WEBP max 1MB</p>
                      <input 
                        type="file" 
                        ref="fileInput" 
