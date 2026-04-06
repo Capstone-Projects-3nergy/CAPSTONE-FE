7050bb4a36bf403173360f2592c04dcdcf8de8c4
นายกฤตภาส จารุบูรณศักดิ์-65130500092 <krittapas22754@gmail.com>
2026-04-06 17:13:47 +0700

    fix

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -504,12 +504,11 @@ const showResetPasswordPageWeb = async function () {
                 ]"
               />
             </div>
-            <div v-if="emailWhitespaceError || emailRequire || emailPasswordRequire" class="flex items-center text-sm text-red-600 mt-1 ml-1">
+            <div v-if="emailWhitespaceError" class="flex items-center text-sm text-red-600 mt-1 ml-1">
               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4 mr-1.5">
                 <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z" clip-rule="evenodd" />
               </svg>
               <span class="text-xs font-medium" v-if="emailWhitespaceError">Please remove leading or trailing spaces.</span>
-              <span class="text-xs font-medium" v-else-if="emailRequire || emailPasswordRequire">Email is required.</span>
             </div>
           </div>
           <div
@@ -594,12 +593,11 @@ const showResetPasswordPageWeb = async function () {
               </svg>
             </button>
           </div>
-          <div v-if="passwordWhitespaceError || passwordRequire || emailPasswordRequire" class="flex items-center text-sm text-red-600 mt-1 ml-1">
+          <div v-if="passwordWhitespaceError" class="flex items-center text-sm text-red-600 mt-1 ml-1">
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-4 h-4 mr-1.5">
               <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zm8.706-1.442c1.146-.573 2.437.463 2.126 1.706l-.709 2.836.042-.02a.75.75 0 01.67 1.34l-.04.022c-1.147.573-2.438-.463-2.127-1.706l.71-2.836-.042.02a.75.75 0 11-.671-1.34l.041-.022zM12 9a.75.75 0 100-1.5.75.75 0 000 1.5z" clip-rule="evenodd" />
             </svg>
             <span class="text-xs font-medium" v-if="passwordWhitespaceError">Please remove leading or trailing spaces.</span>
-            <span class="text-xs font-medium" v-else-if="passwordRequire || emailPasswordRequire">Password is required.</span>
           </div>
         </div>
 
